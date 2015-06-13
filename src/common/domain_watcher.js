ae6a32cecfd0a5e5dc75c1260f489769583a4b70
Olivier Pichon <olivier@netengine.com.au>
2015-06-13 18:23:40 +1000

    Sourcing affliates.json: the files for redirection rules.

--- src/common/domain_watcher.js
+++ src/common/domain_watcher.js
@@ -1,20 +1,39 @@
 // ==UserScript==
 // @require jquery.min.js
+// @include http://*
+// @include https://*
+// ==/UserScript==
 
 function G4G(){
+  var self   = this;
+
   this._init = function(){
     $        = window.$.noConflict(true);
-    var host = this.getCurrentHost();
-    if (host === 'www.amazon.com'){
-      this.showPopup();
-    }
+    this.getSupportedURLs().then(function(data){
+      var host = self.getCurrentHost();
+      $.each(data, function(key, value) {
+        console.log(key);
+        if (host === key){
+          self.showPopup(value);
+        }
+      });
+    });
+  };
+
+  this.getSupportedURLs = function(){
+    var deferred = new $.Deferred();
+    var url      = kango.io.getResourceUrl('res/affiliates.json');
+    $.getJSON(url, function(data) {
+      deferred.resolve(data);
+    });
+    return deferred.promise();
   };
 
   this.getCurrentHost = function(){
     return window.location.host;
   };
 
-  this.showPopup = function(){
+  this.showPopup = function(value){
     var $toolbar = $("<div />").addClass("g4g-popup").appendTo("body");
     console.log('is about to dump Oli in the DOM');
     $toolbar.text('Oliiiiii');
