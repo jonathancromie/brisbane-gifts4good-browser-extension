408ccc527b3bbd4ba00d63c20eb65bf36a400e62
Ricardo Bernardeli <ricardobcs@gmail.com>
2015-06-14 12:03:46 +1000

    Add g4g=true to all urls so that we don’t need to show the popup again

--- csv-to-json.rb
+++ csv-to-json.rb
@@ -1,9 +1,20 @@
 require 'csv'
 require 'json'
 require 'active_support/all'
+require 'uri'
 
 # How to:
 # ruby csv-to-json.rb /path/to/file.csv
+
+def prepare_affiliate_link(url)
+  uri_parsed     = URI.parse(url.gsub("\"", '').split[0].sub(/^https?\:\/\//, ''))
+  query_strings  = (uri_parsed.query || '').split("&")
+  query_strings  << "g4g=true"
+
+  uri_parsed.query = query_strings.join("&")
+
+  uri_parsed.to_s
+end
 
 path = ARGV[0]
 entries = CSV.read(path, { headers: true, converters: :numeric, header_converters: :symbol })
@@ -15,7 +26,7 @@ entries.each do |entry|
   next if entry[:affiliate_link].blank?
   next if entry[:enabled] == "N"
 
-  affiliate_link = entry[:affiliate_link].gsub("\"", '').split[0].sub(/^https?\:\/\//, '')
+  affiliate_link = prepare_affiliate_link(entry[:affiliate_link])
   home_url       = entry[:home_url].sub(/^www./,'').sub(/^https?\:\/\//, '')
 
   base_url = "http://g4gdata.com/gift4good/test.php?link={url}&cause={cause}&UniqueId={uniqueId}&memberID={memberId}"
